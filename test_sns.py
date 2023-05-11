a4ae3193d18a5988dc6261dd2c41711186145b2b
matthijsoudelohuis <matthijsoudelohuis@gmail.com>
2023-05-11 16:19:56 +0100

    test through visual studio code

--- test_sns.py
+++ test_sns.py
@@ -1,6 +1,12 @@
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 
 dots = sns.load_dataset("dots").query("align == 'dots'")
 dots.head()
+
+sns.lineplot(
+    data=dots, x="time", y="firing_rate", hue="coherence", style="choice",
+)
+
+h = 2
 
