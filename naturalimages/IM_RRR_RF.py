f2338a61e982e2e356c8f398d09241fbc64f9840
Matthijs Oude Lohuis <matthijsoudelohuis@gmail.com>
2025-05-12 15:54:54 +0200

    playing around with decoding from different cell populations

--- naturalimages/IM_RRR_RF.py
+++ naturalimages/IM_RRR_RF.py
@@ -7,7 +7,7 @@ Matthijs Oude Lohuis, 2023-2025, Champalimaud Center
 
 #%% 
 import os
-os.chdir('e:\\Python\\molanalysis')
+os.chdir('c:\\Python\\molanalysis')
 from loaddata.get_data_folder import get_local_drive
 
 import numpy as np
@@ -35,6 +35,21 @@ savedir = os.path.join(get_local_drive(),'OneDrive\\PostDoc\\Figures\\Images\\')
 # compute noise correlations for the sessions with 100 images repeated 10 times
 # loop over sessions with all neurons included
 # find regression coeff for each session with enough labeled cells
+# do linear RF fit for each session and with finding optimal lambda
+# fit RF for a subset of trials and reconstruct with held out test trial, 
+# compute R^2 of reconstruction for test data
+# do RF 2D gaussian fit for each session
+# 
+
+# Insights so far: 
+# variaility in sessions how well RF fits are
+# reconstructions and linear RFs are much better in V1, can reconstruct images much better
+# PM and PM labeled is poor
+# dividing by population mean does not help a lot for fitting RF
+# best reconstruction with optimal state: intermediate population size, medium pupil etc.
+
+
+
 
 #%% ################################################
 session_list        = np.array([['LPE11086','2023_12_16']])
@@ -633,11 +648,11 @@ plt.plot(subs,EV_nsubs)
 # slower. Lam depends on df/deconv and needs to be optimized with crossval. Furthermore 
 # lambda biases towards low or high frequency reconstruction. 
 
-sesidx  = 5
+sesidx  = 11
 print(sessions[sesidx].sessiondata['session_id'][0])
-nsub    = 2 #without subsampling really slow, i.e. nsub=1
+nsub    = 3 #without subsampling really slow, i.e. nsub=1
 resp    = sessions[sesidx].respmat.T
-lam     = 0.5
+lam     = 0.05
 
 K,N     = np.shape(resp)
 
@@ -646,7 +661,7 @@ K,N     = np.shape(resp)
 # popcoupling = [np.corrcoef(resp[:,i],poprate)[0,1] for i in range(N)]
 # resp  = resp / np.outer(poprate,popcoupling)
 #alternatively just by dividing by poprate:
-resp = resp / np.mean(resp, axis=1, keepdims=True)
+# resp = resp / np.mean(resp, axis=0, keepdims=True)
 
 #normalize the response for each neuron to the maximum:
 resp = zscore(resp, axis=0)
@@ -866,4 +881,174 @@ fig.tight_layout()
 sns.despine(offset=3,top=True,right=True,trim=True)
 my_savefig(fig,savedir,'R2_pertrial_vs_variables_%s.png' % (sessions[sesidx].sessiondata['session_id'][0]))
 
+#%% 
+sessions[sesidx].celldata = fit_2dgauss_cRF(cRF, nsub=nsub,celldata=sessions[sesidx].celldata)
 
+#%% 
+# Do KNN decoding with choristers and soloists, very easy! Cool! Is there a large V1 dataset with natural images with spiking data? Yes Paolo Papale data. But that is MUA no?
+# Do KNN category decoding? 
+# Reconstruct the image with choristers and soloists, which one better? How about binning sparsity?
+
+sesidx = 11
+resp = sessions[sesidx].respmat.T
+np.shape(resp)
+istim = np.array(sessions[sesidx].trialdata['ImageNumber'])
+nimg = istim.max() # these are blank stims (exclude them)
+
+# mean center each neuron
+resp -= resp.mean(axis=0)
+resp = resp / (resp.std(axis=0) + 1e-6)
+
+### sanity check - decent signal variance ?
+# split stimuli into two repeats
+NN = resp.shape[1]
+sresp = np.zeros((2, nimg, NN), np.float64)
+inan = np.zeros((nimg,)).astype(bool)
+for n in range(nimg):
+    ist = (istim==n).nonzero()[0]
+    i1 = ist[:int(ist.size/2)]
+    i2 = ist[int(ist.size/2):]
+    # check if two repeats of stim
+    if np.logical_or(i2.size < 1, i1.size < 1):
+        inan[n] = 1
+    else:
+        sresp[0, n, :] = resp[i1, :].mean(axis=0)
+        sresp[1, n, :] = resp[i2, :].mean(axis=0)
+        
+# normalize the responses across images
+# Subtract that mean, and divide by std — this makes the response of each neuron
+# for each repeat zero-mean and unit-variance across stimuli.
+# So now, for each neuron, the normalized responses to different stimuli 
+# are on the same scale in both repeats.
+snorm = sresp - sresp.mean(axis=1)[:,np.newaxis,:]
+snorm = snorm / (snorm.std(axis=1)[:,np.newaxis,:] + 1e-6)
+
+#Get the correlation of each neuron's response across repeats
+cc = (snorm[0].T @ snorm[1]) / sresp.shape[1]
+#print the mean correlation coeff:
+print('fraction of signal variance: %2.3f'%np.diag(cc).mean())
+
+cc = sresp[0] @ sresp[1].T
+
+cc = sresp[0] @ sresp[1].T
+cc /= (sresp[0]**2).sum()
+cc /= (sresp[1]**2).sum()
+nstims = sresp.shape[1]
+print('decoding accuracy: %2.3f'%(cc.argmax(axis=1)==np.arange(0,nstims,1,int)).mean())
+
+#%% 
+
+def plot_knn_binnedvar(sessions,sesidx,varlabel,nbins=5):
+
+    resp = sessions[sesidx].respmat.T
+    istim = np.array(sessions[sesidx].trialdata['ImageNumber'])
+    # nimg = istim.max() + 1 # these are blank stims (exclude them)
+    nimg = len(np.unique(istim))
+
+    # mean center each neuron
+    resp -= resp.mean(axis=0)
+    resp = resp / (resp.std(axis=0) + 1e-6)
+
+    ### sanity check - decent signal variance ?
+    # split stimuli into two repeats
+    NN = resp.shape[1]
+    sresp = np.zeros((2, nimg, NN), np.float64)
+    inan = np.zeros((nimg,)).astype(bool)
+    # for n in range(nimg):
+    for n,ni in enumerate(np.unique(istim)): # loop over images
+        ist = (istim==ni).nonzero()[0]
+        i1 = ist[:int(ist.size/2)]
+        i2 = ist[int(ist.size/2):]
+        # check if two repeats of stim
+        if np.logical_or(i2.size < 1, i1.size < 1):
+            inan[n] = 1
+        else:
+            sresp[0, n, :] = resp[i1, :].mean(axis=0)
+            sresp[1, n, :] = resp[i2, :].mean(axis=0)
+    sresp = sresp[:,~inan,:]
+
+    binedges = np.nanpercentile(sessions[sesidx].celldata[varlabel],np.linspace(0,100,nbins+1))
+    bincenters  = (binedges[:-1]+binedges[1:])/2
+    knnperf = np.empty(nbins)
+    for ibin in range(nbins):
+        idx_N = (sessions[sesidx].celldata[varlabel] >= binedges[ibin]) & (sessions[sesidx].celldata[varlabel] < binedges[ibin+1])
+        cc = sresp[0][:,idx_N] @ sresp[1][:,idx_N].T
+        # cc = sresp[0] @ sresp[1].T
+        cc /= (sresp[0,:,idx_N]**2).sum()
+        cc /= (sresp[1,:,idx_N]**2).sum()
+
+        knnperf[ibin] = (cc.argmax(axis=1)==np.arange(0,nimg,1,int)).mean()
+    fig,ax = plt.subplots(1,1,figsize=(3,3))
+    ax.plot(bincenters,knnperf,color='k',linewidth=2)
+    ax.set_xlabel(varlabel)
+    # ax.set_xticks(binedges[:-1])
+    ax.set_ylabel('KNN decoding accuracy')
+    ax.set_ylim([0,ax.get_ylim()[1]])
+    sns.despine(offset=3,top=True,right=True,trim=True)
+
+# plot_knn_binnedvar(sessions,sesidx,varlabel='gini_coefficient',nbins=5)
+# plot_knn_binnedvar(sessions,sesidx,varlabel='skew',nbins=5)
+
+#%%
+sesidx =5
+numeric_cols = sessions[sesidx].celldata.select_dtypes(include=[np.number]).columns
+for col in numeric_cols:
+    plot_knn_binnedvar(sessions,sesidx,varlabel=col,nbins=5)
+
+#%% Add how neurons are coupled to the population rate: 
+for ses in tqdm(sessions,desc='Computing tuning metrics for each session'):
+    resp = zscore(ses.respmat.T,axis=0)
+    poprate = np.mean(resp, axis=1)
+    # popcoupling = [np.corrcoef(resp[:,i],poprate)[0,1] for i in range(N)]
+
+    ses.celldata['pop_coupling']                          = [np.corrcoef(resp[:,i],poprate)[0,1] for i in range(len(ses.celldata))]
+
+plot_knn_binnedvar(sessions,10,varlabel='pop_coupling',nbins=5)
+
+for ises in range(nSessions):
+    sessions[ises].celldata['pop_coupling'][sessions[ises].celldata['pop_coupling']>0.3] = np.nan
+    plot_knn_binnedvar(sessions,ises,varlabel='pop_coupling',nbins=10)
+
+# plot_knn_binnedvar(sessions,11,varlabel='RF_R2',nbins=5)
+
+#%% 
+var1 = 'pop_coupling'
+var2 = 'tuning_SNR'
+# var2 = 'iscell_prob'
+# var2 = 'npix_soma'
+# var2 = 'radius'
+# var2 = 'skew'
+# var2 = 'event_rate'
+
+plt.scatter(sessions[sesidx].celldata[var1],sessions[sesidx].celldata[var2],c='k',alpha=0.5,s=5)
+plt.xlabel(var1)
+plt.ylabel(var2)
+from scipy.stats import linregress
+
+# Linear regression
+slope, intercept, r_value, p_value, std_err = linregress(sessions[sesidx].celldata[var1],sessions[sesidx].celldata[var2])
+
+# Plot regression line
+xs = np.array([sessions[sesidx].celldata[var1].min(),sessions[sesidx].celldata[var1].max()])
+ys = slope * xs + intercept
+plt.plot(xs,ys,'r')
+# plt.axhline(y=1, color='k', linestyle='--', linewidth=1)
+# Plot r value and p value
+plt.text(0.05,0.95,'r=%1.2f, p=%1.2e' % (r_value,p_value),transform=plt.gca().transAxes)
+
+
+#%%
+sesidx = 11
+numeric_cols = ['rf_sx_RRR','rf_sy_RRR','rf_r2_RRR','RF_R2']
+for col in numeric_cols:
+    plot_knn_binnedvar(sessions,sesidx,varlabel=col,nbins=10)
+
+#%%
+sessions[sesidx].celldata['rf_sx_RRR'] = np.clip(sessions[sesidx].celldata['rf_sx_RRR'],0,100)
+sessions[sesidx].celldata['rf_sy_RRR'] = np.clip(sessions[sesidx].celldata['rf_sy_RRR'],0,100)
+
+#%%
+sesidx = 11
+numeric_cols = ['rf_sx_RRR','rf_sy_RRR','rf_r2_RRR','RF_R2']
+for col in numeric_cols:
+    plot_knn_binnedvar(sessions,sesidx,varlabel=col,nbins=5)
